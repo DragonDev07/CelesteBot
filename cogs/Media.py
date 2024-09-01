076d357d3470448fa5c55110dd765d3f55302a99
Teo Welton <teowelton@gmail.com>
2024-08-31 21:51:55 -0600

    Media Queuing Works!

--- cogs/Media.py
+++ cogs/Media.py
@@ -3,6 +3,8 @@ from discord.ext import commands
 from discord import FFmpegPCMAudio
 from pytubefix import YouTube
 import os
+import asyncio
+import time
 
 
 class Media(commands.Cog):
@@ -10,7 +12,7 @@ class Media(commands.Cog):
         self.client = client
         self.queues = {}
 
-    # Log that this cog was loaded
+    # Log that the cog was loaded
     @commands.Cog.listener()
     async def on_ready(self):
         print("The 'Media' cog has been loaded")
@@ -18,11 +20,12 @@ class Media(commands.Cog):
     # Propagate the error to the global error handler
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
-        await self.bot.on_command_error(ctx, error)
+        await self.client.on_command_error(ctx, error)
 
-    # Command that makes the bot join the vc
+    # Command that makes the bot join the voice channel
     @commands.hybrid_command(name="join", description="Join the voice channel")
     async def join(self, ctx):
+        # Check if the author is in a voice channel
         if ctx.author.voice:
             # Get the voice channel of the author
             channel = ctx.message.author.voice.channel
@@ -55,6 +58,7 @@ class Media(commands.Cog):
         # Print that the command was run
         print(f"The 'join' command was run by {ctx.message.author}")
 
+    # Command that makes the bot leave the voice channel
     @commands.hybrid_command(name="leave", description="Leave the voice channel")
     async def leave(self, ctx):
         # Get voice instance
@@ -89,199 +93,6 @@ class Media(commands.Cog):
 
         # Print that the command was run
         print(f"The 'leave' command was run by {ctx.message.author}")
-
-    # Command that pauses the current playing audio
-    @commands.hybrid_command(
-        name="pause", description="Pause the currently playing audio"
-    )
-    async def pause(self, ctx):
-        # Get voice instance
-        voice = discord.utils.get(self.client.voice_clients, guild=ctx.guild)
-
-        # Check if the bot is playing audio
-        if voice.is_playing():
-            # Pause the audio
-            voice.pause()
-
-            # Send a response embed to the command user
-            embed = discord.Embed(
-                title="Paused Audio",
-                description="Paused Audio",
-                color=discord.Color.green(),
-            )
-            embed.set_footer(
-                text=f"`pause` command was run by {ctx.message.author}",
-                icon_url=ctx.author.avatar,
-            )
-
-            await ctx.send(embed=embed)
-        else:
-            # If the bot is not playing audio, send a response embed to the command user
-            embed = discord.Embed(
-                title="Error",
-                description="I am not playing anything at the moment",
-                color=discord.Color.red(),
-            )
-            await ctx.send(embed=embed)
-
-        # Print that the command was run
-        print(f"The 'pause' command was run by {ctx.message.author}")
-
-    # Command that resumes paused audio
-    @commands.hybrid_command(
-        name="resume", description="Resume the currently paused audio"
-    )
-    async def resume(self, ctx):
-        # Get voice instance
-        voice = discord.utils.get(self.client.voice_clients, guild=ctx.guild)
-
-        # Check if the bot is paused
-        if voice.is_paused():
-            # Resume the audio
-            voice.resume()
-
-            # Send a response embed to the command user
-            embed = discord.Embed(
-                title="Resumed Audio",
-                color=discord.Color.green(),
-            )
-            embed.set_footer(
-                text=f"`resume` command was run by {ctx.message.author}",
-                icon_url=ctx.author.avatar,
-            )
-
-            await ctx.send(embed=embed)
-        else:
-            # If the bot is not paused, send a response embed to the command user
-            embed = discord.Embed(
-                title="Error",
-                description="No audio is paused at the moment!",
-                color=discord.Color.red(),
-            )
-
-            await ctx.send(embed=embed)
-
-        # Print that the command was run
-        print(f"The 'resume' command was run by {ctx.message.author}")
-
-    # Command that playes given YouTube URL
-    @commands.hybrid_command(name="play", description="Play audio from a YouTube URL")
-    async def play(self, ctx, url):
-        # Defer the response
-        await ctx.defer()
-
-        # Initialize the queue for the guild if it doesn't exist
-        if ctx.guild.id not in self.queues:
-            self.queues[ctx.guild.id] = []
-
-        # Get voice instance
-        voice = discord.utils.get(self.client.voice_clients, guild=ctx.guild)
-
-        # Check if the bot is in a voice channel
-        if not voice:
-            # If the bot is not in a voice channel, send a response embed to the command user
-            embed = discord.Embed(
-                title="Error",
-                description="I'm not connected to a voice channel. Use /join to add me to one!",
-                color=discord.Color.red(),
-            )
-
-            await ctx.send(embed=embed)
-            return
-
-        # Add the URL to the queue
-        self.queues[ctx.guild.id].append(url)
-
-        # Send an embed that the URL was added to the queue to the command user
-        embed = discord.Embed(
-            title="Added to Queue",
-            description=f"Added: {url} to the queue",
-            color=discord.Color.green(),
-        )
-        embed.set_footer(
-            text=f"'play' command was run by {ctx.message.author}",
-            icon_url=ctx.author.avatar,
-        )
-
-        await ctx.send(embed=embed)
-
-        # Play the next song in the queue
-        await self.play_next(ctx)
-
-        # Print that the command was run
-        print(
-            f"The 'play' command was run by {ctx.message.author}, adding video {url} to the queue"
-        )
-
-    async def play_next(self, ctx):
-        if ctx.guild.id not in self.queues or not self.queues[ctx.guild.id]:
-            return
-
-        url = self.queues[ctx.guild.id].pop(0)
-
-        # Get voice instance
-        voice = discord.utils.get(self.client.voice_clients, guild=ctx.guild)
-
-        # Get youtube video
-        yt = YouTube(url)
-        stream = yt.streams.filter(only_audio=True).first()
-        stream.download(output_path="tmp", filename="temp_audio.mp3")
-
-        # Play downloaded audio file
-        path = "tmp/temp_audio.mp3"
-        voice.play(discord.FFmpegPCMAudio(path), after=lambda x: end_song(ctx, path))
-        voice.source = discord.PCMVolumeTransformer(voice.source, 1)
-
-        # Send a response embed to the command user
-        embed = discord.Embed(
-            title="Playing Audio",
-            description=f"Playing: {url}",
-            color=discord.Color.green(),
-        )
-        embed.set_footer(
-            text=f"'play' command was run by {ctx.message.author}",
-            icon_url=ctx.author.avatar,
-        )
-
-        await ctx.channel.send(embed=embed)
-
-    def end_song(self, ctx, path):
-        os.remove(path)
-        # Play the next song in the queue
-        self.client.loop.create_task(self.play_next(ctx))
-
-    # Command that stops playing audio (deletes file, cannot be resumed)
-    @commands.hybrid_command(
-        name="stop", description="Stop playing audio (cannot be resumed)"
-    )
-    async def stop(self, ctx):
-        # Defer the response
-        await ctx.defer()
-
-        # Get voice instance
-        voice = discord.utils.get(self.client.voice_clients, guild=ctx.guild)
-
-        # Pause the audio
-        voice.pause()
-
-        # End the song
-        end_song(ctx.guild, "tmp/temp_audio.mp3")
-
-        # Send a response embed to the command user
-        embed = discord.Embed(
-            title="Stopped Audio",
-            description="Stopped Audio",
-            color=discord.Color.green(),
-        )
-        embed.set_footer(
-            text=f"`stop` command was run by {ctx.message.author}",
-            icon_url=ctx.author.avatar,
-        )
-
-        await ctx.send(embed=embed)
-
-        # Print that the command was run
-        print(f"The 'stop' command was run by {ctx.message.author}")
 
     # Command to get or set volume of the audio
     @commands.hybrid_command(
@@ -348,6 +159,283 @@ class Media(commands.Cog):
         print(
             f"The 'volume' command was run by {ctx.message.author} to set the volume to {percent}%"
         )
+
+    # Command that pauses the currently playing audio
+    @commands.hybrid_command(
+        name="pause", description="Pause the currently playing audio"
+    )
+    async def pause(self, ctx):
+        # Get voice instance
+        voice = discord.utils.get(self.client.voice_clients, guild=ctx.guild)
+
+        # Check if the bot is playing audio
+        if voice.is_playing():
+            # Pause the audio
+            voice.pause()
+
+            # Send a response embed to the command user
+            embed = discord.Embed(
+                title="Paused Audio",
+                description="Paused Audio",
+                color=discord.Color.green(),
+            )
+            embed.set_footer(
+                text=f"`pause` command was run by {ctx.message.author}",
+                icon_url=ctx.author.avatar,
+            )
+
+            await ctx.send(embed=embed)
+        else:
+            # If the bot is not playing audio, send a response embed to the command user
+            embed = discord.Embed(
+                title="Error",
+                description="I am not playing anything at the moment",
+                color=discord.Color.red(),
+            )
+
+            await ctx.send(embed=embed)
+
+        # Print that the command was run
+        print(f"The 'pause' command was run by {ctx.message.author}")
+
+    # Command that resumes the currently paused audio
+    @commands.hybrid_command(
+        name="resume", description="Resume the currently paused audio"
+    )
+    async def resume(self, ctx):
+        # Get voice instance
+        voice = discord.utils.get(self.client.voice_clients, guild=ctx.guild)
+
+        # Check if the bot is paused
+        if voice.is_paused():
+            # Resume the audio
+            voice.resume()
+
+            # Send a response embed to the command user
+            embed = discord.Embed(
+                title="Resumed Audio",
+                color=discord.Color.green(),
+            )
+            embed.set_footer(
+                text=f"`resume` command was run by {ctx.message.author}",
+                icon_url=ctx.author.avatar,
+            )
+
+            await ctx.send(embed=embed)
+        else:
+            # If the bot is not paused, send a response embed to the command user
+            embed = discord.Embed(
+                title="Error",
+                description="No audio is paused at the moment!",
+                color=discord.Color.red(),
+            )
+
+            await ctx.send(embed=embed)
+
+        # Print that the command was run
+        print(f"The 'resume' command was run by {ctx.message.author}")
+
+    # Command that skips the currently playing audio
+    @commands.hybrid_command(
+        name="skip", description="Skip the currently playing audio"
+    )
+    async def skip(self, ctx):
+        # Defer the response
+        await ctx.defer()
+
+        # Get voice instance
+        voice = discord.utils.get(self.client.voice_clients, guild=ctx.guild)
+
+        if voice:
+            # Stop the audio
+            voice.stop()
+
+            # End the song and play the next one
+            await self.end_song(ctx, ctx.guild, "tmp/temp_audio.mp3")
+
+            # Send a response embed to the command user
+            embed = discord.Embed(
+                title="Skipped Audio",
+                description="Skipped Audio",
+                color=discord.Color.green(),
+            )
+            embed.set_footer(
+                text=f"`skip` command was run by {ctx.message.author}",
+                icon_url=ctx.author.avatar,
+            )
+
+            await ctx.send(embed=embed)
+        else:
+            # If the bot is not playing audio, send a response embed to the command user
+            embed = discord.Embed(
+                title="Error",
+                description="I am not playing anything at the moment",
+                color=discord.Color.red(),
+            )
+
+            await ctx.send(embed=embed)
+
+        # Print that the command was run
+        print(f"The 'skip' command was run by {ctx.message.author}")
+
+    # Command that stops the currently playing audio
+    @commands.hybrid_command(
+        name="stop", description="Stop playing audio (cannot be resumed)"
+    )
+    async def stop(self, ctx):
+        # Defer the response
+        await ctx.defer()
+
+        # Clear the queue
+        self.queues[ctx.guild.id] = []
+
+        # Get voice instance
+        voice = discord.utils.get(self.client.voice_clients, guild=ctx.guild)
+
+        # Check if the bot has a voice instance
+        if voice:
+            # Stop the audio
+            voice.stop()
+
+            # Send a response embed to the command user
+            embed = discord.Embed(
+                title="Stopped Audio",
+                description="Stopped Audio",
+                color=discord.Color.green(),
+            )
+            embed.set_footer(
+                text=f"`stop` command was run by {ctx.message.author}",
+                icon_url=ctx.author.avatar,
+            )
+
+            await ctx.send(embed=embed)
+        else:
+            # If the bot is not playing audio, send a response embed to the command user
+            embed = discord.Embed(
+                title="Error",
+                description="I am not playing anything at the moment",
+                color=discord.Color.red(),
+            )
+
+            await ctx.send(embed=embed)
+
+        # Print that the command was run
+        print(f"The 'stop' command was run by {ctx.message.author}")
+
+    # Command that plays audio from a YouTube URL
+    @commands.hybrid_command(name="play", description="Play audio from a YouTube URL")
+    async def play(self, ctx, url):
+        # Defer the response
+        await ctx.defer()
+
+        # Get the guild ID
+        guild_id = ctx.guild.id
+
+        # Check if the guild ID is in the queues
+        if guild_id not in self.queues:
+            # If not, add it
+            self.queues[guild_id] = []
+
+        # Append the URL to the queue
+        self.queues[guild_id].append(url)
+
+        # Get the voice instance
+        voice = discord.utils.get(self.client.voice_clients, guild=ctx.guild)
+
+        # Check if the bot is connected to a voice channel
+        if not voice:
+            # If not, send a response embed to the command user
+            embed = discord.Embed(
+                title="Error",
+                description="I'm not connected to a voice channel. Use /join to add me to one!",
+                color=discord.Color.red(),
+            )
+
+            await ctx.send(embed=embed)
+            return
+
+        # Check if the bot is playing audio
+        if not voice.is_playing():
+            await self.play_next(ctx)
+
+        # Send a response embed to the command user
+        embed = discord.Embed(
+            title="Added to Queue",
+            description=f"Added to queue: {url}",
+            color=discord.Color.green(),
+        )
+        embed.set_footer(
+            text=f"'play' command was run by {ctx.message.author}",
+            icon_url=ctx.author.avatar,
+        )
+
+        await ctx.send(embed=embed)
+
+        # Print that the command was run
+        print(
+            f"The `play` command was run by {ctx.message.author}, added video {url} to queue"
+        )
+
+    # Function that plays the next audio in the queue
+    async def play_next(self, ctx):
+        # Get the guild ID
+        guild_id = ctx.guild.id
+
+        # Check if the guild ID is in the queues and if there are any URLs in the queue
+        if guild_id in self.queues and self.queues[guild_id]:
+            # Get the first URL in the queue
+            url = self.queues[guild_id].pop(0)
+
+            # Get the guild
+            guild = ctx.message.guild
+
+            # Get the voice instance
+            voice = discord.utils.get(self.client.voice_clients, guild=ctx.guild)
+
+            # Download the audio from the URL
+            yt = YouTube(url)
+            stream = yt.streams.filter(only_audio=True).first()
+            stream.download(output_path="tmp", filename="temp_audio.mp3")
+
+            # Play the audio
+            path = "tmp/temp_audio.mp3"
+            voice.play(
+                discord.FFmpegPCMAudio(path),
+                after=lambda e: self.end_song(ctx, guild, path),
+            )
+
+            # Set the volume of the audio
+            voice.source = discord.PCMVolumeTransformer(voice.source, 1)
+
+            # Send a response embed to the command user
+            embed = discord.Embed(
+                title="Playing Audio",
+                description=f"Playing: {url}",
+                color=discord.Color.green(),
+            )
+            embed.set_footer(
+                text=f"'play' command was run by {ctx.message.author}",
+                icon_url=ctx.author.avatar,
+            )
+
+            await ctx.send(embed=embed)
+
+            # Print that the audio is playing
+            print(f"Playing video {url} in {guild}")
+
+    # Function that ends the currently playing audio
+    def end_song(self, ctx, guild, path):
+        # Get the voice instance
+        os.remove(path)
+
+        # Sleep to prevent FFMPEG from crashing
+        time.sleep(2)
+
+        future = asyncio.run_coroutine_threadsafe(self.play_next(ctx), self.client.loop)
+        try:
+            future.result()
+        except Exception as e:
+            print(f"Error occurred while trying to play the next song: {e}")
 
 
 async def setup(client):
